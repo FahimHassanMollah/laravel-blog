1f70f63a389733e106a03b2537ba845a251b005f
FahimHassanMollah <fahimhassan0197@gmail.com>
2021-10-28 11:40:12 +0600

    class 3 completed

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+   public function index()
+   {
+       return view('home.home');
+   }
+   public function post()
+   {
+    return view('partials.post');
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontEndController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/',[FrontEndController::class,'index']);
+Route::get('/post',[FrontEndController::class,'post']);
